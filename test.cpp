2263dacf5fa18b82ab4f4b2fd7289b41c49c00f2
xatify <abbouzid@student.1337.ma>
2022-02-14 23:15:48 +0100

    lexicographical compare added

--- test.cpp
+++ test.cpp
@@ -81,6 +81,18 @@ void test (C    v) {
 	v.pop_back ();
 	print (v);
 
+	// lexicographical compare
+
+	C foo (3,100);   // three ints with a value of 100
+  	C bar (2,200);   // two ints with a value of 200
+
+	if (foo==bar) std::cout << "foo and bar are equal\n";
+	if (foo!=bar) std::cout << "foo and bar are not equal\n";
+	if (foo< bar) std::cout << "foo is less than bar\n";
+	if (foo> bar) std::cout << "foo is greater than bar\n";
+	if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+	if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
 }
 
 int main () {
