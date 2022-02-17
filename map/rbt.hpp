67a224357a1bd116b7282ef5c3ff405982628d79
xatify <abbouzid@student.1337.ma>
2022-02-17 06:57:41 +0100

    some functionality are not tested

--- map/rbt.hpp
+++ map/rbt.hpp
@@ -2,6 +2,7 @@
 #define	__RBT_HPP__
 
 #include <memory>
+#include <iostream>
 
 template <	typename Key,
 			typename T
@@ -9,21 +10,21 @@ template <	typename Key,
 struct node {
 	typedef std::pair<Key, T> 	value_type;
 	enum Color { RED, BLACK }	color;
-	std::pair<Key, T>			value;
+	
+	value_type					value;
 	node						*left,
 								*right,
 								*parent;
 
-	node (): c(BLACK), left (0x0), right (0x0), parent (0x0) {};
+	node (): color (BLACK), left (0x0), right (0x0), parent (0x0) {};
 	node (const value_type& v): value (v), left (0x0), right (0x0), parent (0x0) {};
 	Key& key () { return value.first; };
 };
 
 template <	typename Key,
 			typename T,
-			typename Compare = std::less<Key>,
-			typename Alloc = std::allocator<node<Key, T> >
-			>
+			typename Compare = std::less<Key>
+		>
 class RBT {
 
 	//****************************************************************************
@@ -41,32 +42,66 @@ class RBT {
 	//	the same number of black nodes (black hight)
 	//*****************************************************************************
 
-	typedef Alloc							allocator_type;
-	typedef node<T, Key>					node_type;
-	typedef typename node_type::Color		Color;
-	typedef typename node_type::value_type	value_type;
-	typedef typename value_type::BLACK		BLACK;
-	typedef typename value_type::BLACK		RED;
-	typedef node_type *						pointer;
-	typedef Key								key_type;
-	typedef	Compare							key_compare;
+	public:
+		typedef std::allocator<node<Key, T> >	allocator_type;
+		typedef node<Key, T>					node_type;
+		typedef typename node_type::Color		Color;
+		typedef typename node_type::value_type	value_type;
+		typedef node_type *						pointer;
+		typedef Key								key_type;
+		typedef	Compare							key_compare;
+		
 	
 	public:
-		RBT (const key_compare& comp_ = key_compare () , 
-			const allocator_type& alloc_ = allocator_type ()): comp (comp_), alloc (alloc_), ROOT (0x0) {
+
+		RBT (const key_compare& comp_ = key_compare ()): comp (comp_), alloc (allocator_type ()) {
 			NIL = alloc.allocate (1);
 			alloc.construct (NIL, node_type ());
 			ROOT = NIL;
 		}
-		~RBT () {
-			alloc.destroy (NIL); alloc.deallocate (NIL, 1);
+
+		RBT& operator = (const RBT& x) {
+			if (this != &x) {
+				comp = x.comp;
+				alloc = x.alloc;
+				this->clear (ROOT);
+				if (x.ROOT == x.NIL)
+					ROOT = NIL;
+				else {
+					ROOT = x.clone (x.ROOT, NIL);
+					ROOT->parent = NIL;
+				}
+			}
+			return (*this);
 		}
 
-		// returns the node with the key k if found otherwise returns NIL
-		pointer		search (pointer x, key_type& k) {
+		~RBT () {
+			clear (ROOT);
+			alloc.destroy (NIL);
+			alloc.deallocate (NIL, 1);
+		}
+
+		// clone a new tree from the subtree rooted at x
+		// and use new_nil as the new sentinel in case
+		// we have different trees 
+		pointer clone (pointer x, pointer new_nil) {
+			if (x == NIL)
+				return new_nil;
+			pointer clone = alloc.allocate (1);
+			alloc.construct (clone, *x);
+			clone->left = clone (x->left, new_nil);
+			clone->left->parent = clone;
+			clone->right = clone (x->right, new_nil);
+			clone->right->parent = clone;
+			return x;
+		}
+
+		// search the tree for a node with key k
+		bool	search (key_type k) {
+			pointer x = ROOT;
 			while (x != NIL && k != x->key ())
 				x = (comp (k, x->key ())) ? x->left: x->right;
-			return x;
+			return  (x != NIL);
 		}
 
 		// returns the deepest left node in sub-binay tree rooted at x
@@ -108,7 +143,7 @@ class RBT {
 		
 		void left_rotate (pointer x) {							// left rotate operation 			
 			pointer y = x->right;								// assuming that x.right != nil and the root's parent is nil
-			x->right = y.left;									// and the root's parent is nil
+			x->right = y->left;									// and the root's parent is nil
 			if (y->left != NIL)
 				y->left->parent = x;
 			y->parent = x->parent;
@@ -121,9 +156,6 @@ class RBT {
 			y->left = x;
 			x->parent = y;
 		}
-
-		
-		
 
 		void right_rotate (pointer y) {							// right rotate operation
 			pointer x = y->left;								// assuming that y.left != nil and the root's parent is nil
@@ -150,13 +182,13 @@ class RBT {
 
 		void	insert (const value_type& v) {
 			pointer z = alloc.allocate (1);
-			alloc.construct (z, node (v));
+			alloc.construct (z, node_type (v));
 			
 			pointer y = NIL;
 			pointer x = ROOT;
 			while (x != NIL) {
 				y = x;
-				x = (comp (z->key (), x->key ()) ? x->left: x->right;
+				x = (comp (z->key (), x->key ())) ? x->left: x->right;
 			}
 			z->parent = y;
 			if (y == NIL)
@@ -168,7 +200,7 @@ class RBT {
 			
 			z->left = NIL;
 			z->right = NIL;
-			z->color = RED;
+			z->color = node_type::RED;
 			this->fixup (z);
 		}
 
@@ -180,31 +212,31 @@ class RBT {
 		// 
 		
 		void	fixup (pointer z) {
-			while (z->parent->color == RED) {
+			while (z->parent->color == node_type::RED) {
 				if (z->parent == z->parent->parent->left) {
-					pointer y = z->parent->parent->right;		// y the uncle of z
-					if (y->color == RED) {						// case 1: the color of y is red
-						z->parent->color = BLACK;				// we color z.parent and z.uncle in black
-						y->color = BLACK;						// and z.p.p in red, this might violate property 4 between 
-						z->parent->parent->color = RED;			// z.p.p and z.p.p.p so we set z = z.p.p 
-						z = z->parent->parent;					// to restore it in the next iteration (we go up 2 levels)
+					pointer y = z->parent->parent->right;					// y the uncle of z
+					if (y->color == node_type::RED) {						// case 1: the color of y is red
+						z->parent->color = node_type::BLACK;				// we color z.parent and z.uncle in black
+						y->color = node_type::BLACK;						// and z.p.p in red, this might violate property 4 between 
+						z->parent->parent->color = node_type::RED;			// z.p.p and z.p.p.p so we set z = z.p.p 
+						z = z->parent->parent;								// to restore it in the next iteration (we go up 2 levels)
 					}
 					else { 
-						if (z == z->parent->right) {			// case 2: y is black and z is the right child
-							z = z->parent;						// we set z = z.p
-							left_rotate (z);					// left rotate z to get case 3
+						if (z == z->parent->right) {						// case 2: y is black and z is the right child
+							z = z->parent;									// we set z = z.p
+							left_rotate (z);								// left rotate z to get case 3
 						}					
-						z->parent->color = BLACK;				// case 3: y is black and z is the left child
-						z->parent->parent->color = RED;			// then right rotate right z.p.p
-						right_rotate (z->parent->parent);		// now the property 4 is maintained 
+						z->parent->color = node_type::BLACK;				// case 3: y is black and z is the left child
+						z->parent->parent->color = node_type::RED;			// then right rotate right z.p.p
+						right_rotate (z->parent->parent);					// now the property 4 is maintained 
 					}
 				}
 				else {
-					pointer y = z->parent->parent->left;		// same as the above, except 
-					if (y->color == RED) {						// the uncle y is in the left
-						z->parent->color = BLACK;
-						y->color = BLACK;
-						z->parent->parent->color = RED;
+					pointer y = z->parent->parent->left;					// same as the above, except 
+					if (y->color == node_type::RED) {						// the uncle y is in the left
+						z->parent->color = node_type::BLACK;
+						y->color = node_type::BLACK;
+						z->parent->parent->color = node_type::RED;
 						z = z->parent->parent;
 					}
 					else {
@@ -212,13 +244,13 @@ class RBT {
 							z = z->parent;
 							right_rotate (z);
 						}
-						z->parent->color = BLACK;
-						y->parent->parent->color = RED;
+						z->parent->color = node_type::BLACK;
+						y->parent->parent->color = node_type::RED;
 						left_rotate (z->parent->parent);
 					}
 				}
 			}
-			ROOT->color = BLACK;								// always fix the second property 
+			ROOT->color = node_type::BLACK;										// always fix the second property 
 		}
 
 		
@@ -235,7 +267,7 @@ class RBT {
 		}
 
 		// delete a node z from the RBT
-		void	delete (pointer z) {
+		void	remove (pointer z) {
 			pointer y = z;
 			Color y_original_color = y->color;
 			pointer x;
@@ -248,7 +280,7 @@ class RBT {
 				this->transplant (z, z->left);
 			}
 			else {
-				y = this->extrem_left (z->right)
+				y = this->extrem_left (z->right);
 				y_original_color = y->color;
 				x = y->right;
 				if (y->parent == z)
@@ -263,65 +295,95 @@ class RBT {
 				y->left->parent = y;
 				y->color = z->color;
 			}
-			if (y_original_color == BLACK)
-				this->delete_fixeup (x);
+			if (y_original_color == Color::BLCK)
+				this->remove_fixeup (x);
+			alloc.destroy (z);
+			alloc.deallocate (z, 1);
 		}
 
-		void	delete_fixup (pointer x) {
-			while (x != NIL && x->color == BLACK) {
+		void	remove_fixup (pointer x) {
+			while (x != NIL && x->color == node_type::BLACK) {
 				if (x == x->parent->left) {
 					pointer w = x->parent->right;
-					if (w->color == RED) {
-						w->color = BLACK;
-						x->parent->color = RED;
+					if (w->color == node_type::RED) {
+						w->color = node_type::BLACK;
+						x->parent->color = node_type::RED;
 						this->left_rotate (x->parent);
 						w = x->parent->right;
 					}
-					if (w->left->color == BLACK && w->right->color == BLACK) {
-						w->color = RED;
+					if (w->left->color == node_type::BLACK && w->right->color == node_type::BLACK) {
+						w->color = node_type::RED;
 						x = x->parent;
 					}
 					else { 
-						if (w->right->color == BLACK) {
-							w->left->color = BLACK;
-							w->color = RED;
+						if (w->right->color == node_type::BLACK) {
+							w->left->color = node_type::BLACK;
+							w->color = node_type::RED;
 							this->right_rotate (w);
 							w = x->parent->right;
 						}
 						w->color = x->parent->color;
-						x->parent->color = BLACK;
-						w->right->color = BLACK;
+						x->parent->color = node_type::BLACK;
+						w->right->color = node_type::BLACK;
 						this->left_rotate (x->parent);
 						x = ROOT;
 					}
 				}
 				else {
 					pointer w = x->parent->left;
-					if (w->color == RED) {
-						w->color = BLACK;
-						x->parent->color = RED;
+					if (w->color == node_type::RED) {
+						w->color = node_type::BLACK;
+						x->parent->color = node_type::RED;
 						this->right_rotate (x->parent);
 						w = x->parent->left;
 					}
-					if (w->right->color == BLACK && w->left->color == BLACK) {
-						w->color = RED;
+					if (w->right->color == node_type::BLACK && w->left->color == node_type::BLACK) {
+						w->color = node_type::RED;
 						x = x->parent;
 					}
 					else {
-						if (w->left->color == BLACK) {
-							w->right->color = BLACK;
-							w->color = RED;
+						if (w->left->color == node_type::BLACK) {
+							w->right->color = node_type::BLACK;
+							w->color = node_type::RED;
 							this->left_rotate (w);
 							w = x->parent->parent;
 						}
 						w->color = x->parent->color;
-						x->parent->color = BLACK;
+						x->parent->color = node_type::BLACK;
 						this->right_rotate (x->parent);
 						x = ROOT;
 					}
 				}
 			}
-			x->color = BLACK;
+			x->color = node_type::BLACK;
+		}
+
+		// capacity functions
+		bool empty () { return ROOT == NIL; }
+
+		// number of nodes in the tree
+		size_t size () const {
+			return size (ROOT);
+		}
+
+
+		// count recursively the number of nodes in the subtree
+		// rooted at x
+		size_t size (pointer x) const {
+			if (x == NIL) return 0;
+			return (size (x->right) + size (x->left));
+		}
+ 
+
+		void clear () { clear (ROOT); }									//delete all the elements
+	
+		void clear (pointer x) {										// delete the subtree rooted at x  except nil								
+			if (x != NIL) {
+				clear (x->left);
+				clear (x->right);
+				alloc.destroy (x);
+				alloc.deallocate (x, 1);
+			}
 		}
 
  	private:
