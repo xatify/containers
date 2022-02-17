748660e0e7a9ceb4f5170f395ba53b0d8d371c91
xatify <abbouzid@student.1337.ma>
2022-02-17 20:10:44 +0100

    adding const T* specialization

--- utility/iterator_traits.hpp
+++ utility/iterator_traits.hpp
@@ -22,6 +22,14 @@ namespace ft {
 		typedef		std::random_access_iterator_tag		iterator_category;
 	};
 
+	template <typename T>
+	struct iterator_traits <const T*> {
+		typedef		ptrdiff_t							difference_type;
+		typedef		T									value_type;
+		typedef 	const T*							pointer;
+		typedef 	const T&							reference;
+		typedef		std::random_access_iterator_tag		iterator_categoty;
+	};
 }
 
 #endif
